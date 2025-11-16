e896037f81a86b96bedd73f40a64ff5661feb604
bri-murphy <brian.murphy431@gmail.com>
2025-11-16 11:17:36 -0500

    adding file writing - CH2L4

---
+++ functions/write_file.py
@@ -0,0 +1,13 @@
+import os
+from config import *
+def write_file(working_directory, file_path, content):
+    work_abs = os.path.abspath(working_directory)
+    abs_path = os.path.abspath(os.path.join(working_directory, file_path))
+    if not (abs_path == work_abs or abs_path.startswith(work_abs + os.sep)):
+        return f'Error: Cannot list "{file_path}" as it is outside the permitted working directory'
+    try:
+        with open(abs_path, "w") as f:
+            f.write(content)
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    except Exception as e:
+        return f'Error: {e}'
--- tests.py
+++ tests.py
@@ -1,9 +1,7 @@
-from functions.get_file_content import get_file_content
+from functions.write_file import write_file
 print('Test Case 1:')
-print(get_file_content("calculator", "main.py"))
+print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
 print('Test Case 2:')
-print(get_file_content("calculator", "pkg/calculator.py"))
+print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
 print('Test Case 3:')
-print(get_file_content("calculator", "/bin/cat")) #(this should return an error string)
-print('Test Case 4:')
-print(get_file_content("calculator", "pkg/does_not_exist.py")) #(this should return an error string)
+print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
